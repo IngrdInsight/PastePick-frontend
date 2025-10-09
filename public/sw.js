0013f8dcee0db87dfe42528e0a3e3c5f9d694cce
t2baak00 <t2baak00@students.oamk.fi>
2025-10-09 12:44:29 +0300

    feat: pwa working

---
+++ public/sw.js
@@ -0,0 +1,449 @@
+if (!self.define) {
+  let e,
+    s = {};
+  const n = (n, a) => (
+    (n = new URL(n + ".js", a).href),
+    s[n] ||
+      new Promise((s) => {
+        if ("document" in self) {
+          const e = document.createElement("script");
+          (e.src = n), (e.onload = s), document.head.appendChild(e);
+        } else (e = n), importScripts(n), s();
+      }).then(() => {
+        let e = s[n];
+        if (!e) throw new Error(`Module ${n} didn’t register its module`);
+        return e;
+      })
+  );
+  self.define = (a, i) => {
+    const c =
+      e ||
+      ("document" in self ? document.currentScript.src : "") ||
+      location.href;
+    if (s[c]) return;
+    let t = {};
+    const r = (e) => n(e, c),
+      f = { module: { uri: c }, exports: t, require: r };
+    s[c] = Promise.all(a.map((e) => f[e] || r(e))).then((e) => (i(...e), t));
+  };
+}
+define(["./workbox-3c9d0171"], function (e) {
+  "use strict";
+  importScripts("/fallback-ce627215c0e4a9af.js"),
+    self.skipWaiting(),
+    e.clientsClaim(),
+    e.precacheAndRoute(
+      [
+        {
+          url: "/_next/static/chunks/408-5c2c83f00b109e58.js",
+          revision: "5c2c83f00b109e58",
+        },
+        {
+          url: "/_next/static/chunks/658-d90ed20e8504b892.js",
+          revision: "d90ed20e8504b892",
+        },
+        {
+          url: "/_next/static/chunks/71.012f8e90c2d8bb5c.js",
+          revision: "012f8e90c2d8bb5c",
+        },
+        {
+          url: "/_next/static/chunks/79637b0d-2f8e7221e928ce00.js",
+          revision: "2f8e7221e928ce00",
+        },
+        {
+          url: "/_next/static/chunks/810.2a62262c158fdaff.js",
+          revision: "2a62262c158fdaff",
+        },
+        {
+          url: "/_next/static/chunks/977-c767bd7f6587c67c.js",
+          revision: "c767bd7f6587c67c",
+        },
+        {
+          url: "/_next/static/chunks/app/_not-found/page-44566510b58bc816.js",
+          revision: "44566510b58bc816",
+        },
+        {
+          url: "/_next/static/chunks/app/layout-8066fa1624ac3ac3.js",
+          revision: "8066fa1624ac3ac3",
+        },
+        {
+          url: "/_next/static/chunks/app/page-e11db09ac02f18ba.js",
+          revision: "e11db09ac02f18ba",
+        },
+        {
+          url: "/_next/static/chunks/framework-b2916a93df015e89.js",
+          revision: "b2916a93df015e89",
+        },
+        {
+          url: "/_next/static/chunks/main-823ab3e51673b056.js",
+          revision: "823ab3e51673b056",
+        },
+        {
+          url: "/_next/static/chunks/main-app-b1733cae09e6a2aa.js",
+          revision: "b1733cae09e6a2aa",
+        },
+        {
+          url: "/_next/static/chunks/pages/_app-5d1c5f5f9d0142ec.js",
+          revision: "5d1c5f5f9d0142ec",
+        },
+        {
+          url: "/_next/static/chunks/pages/_error-19b8ad5779157c1f.js",
+          revision: "19b8ad5779157c1f",
+        },
+        {
+          url: "/_next/static/chunks/polyfills-42372ed130431b0a.js",
+          revision: "846118c33b2c0e922d7b3a7676f81f6f",
+        },
+        {
+          url: "/_next/static/chunks/webpack-2b0b0189e372f936.js",
+          revision: "2b0b0189e372f936",
+        },
+        {
+          url: "/_next/static/css/2cdc926594607df2.css",
+          revision: "2cdc926594607df2",
+        },
+        {
+          url: "/_next/static/jt21rPV_ANoYZ5LR6NZTb/_buildManifest.js",
+          revision: "eee9dfce5904a37c3dcc39e3fc0b2609",
+        },
+        {
+          url: "/_next/static/jt21rPV_ANoYZ5LR6NZTb/_ssgManifest.js",
+          revision: "b6652df95db52feb4daf4eca35380933",
+        },
+        {
+          url: "/_next/static/media/19cfc7226ec3afaa-s.woff2",
+          revision: "9dda5cfc9a46f256d0e131bb535e46f8",
+        },
+        {
+          url: "/_next/static/media/21350d82a1f187e9-s.woff2",
+          revision: "4e2553027f1d60eff32898367dd4d541",
+        },
+        {
+          url: "/_next/static/media/8e9860b6e62d6359-s.woff2",
+          revision: "01ba6c2a184b8cba08b0d57167664d75",
+        },
+        {
+          url: "/_next/static/media/ba9851c3c22cd980-s.woff2",
+          revision: "9e494903d6b0ffec1a1e14d34427d44d",
+        },
+        {
+          url: "/_next/static/media/c5fe6dc8356a8c31-s.woff2",
+          revision: "027a89e9ab733a145db70f09b8a18b42",
+        },
+        {
+          url: "/_next/static/media/df0a9ae256c0569c-s.woff2",
+          revision: "d54db44de5ccb18886ece2fda72bdfe0",
+        },
+        {
+          url: "/_next/static/media/e4af272ccee01ff0-s.p.woff2",
+          revision: "65850a373e258f1c897a2b3d75eb74de",
+        },
+        {
+          url: "/apple-touch-icon.png",
+          revision: "11d89ccf72f1ffa20ae42019f7d2cead",
+        },
+        {
+          url: "/fallback-ce627215c0e4a9af.js",
+          revision: "1ad32685aa237a1203d900379c1867b8",
+        },
+        {
+          url: "/favicon-96x96.png",
+          revision: "58080a7b8537a8942248beb5c619cb3d",
+        },
+        { url: "/favicon.ico", revision: "810661b5e792838af369131493f6562f" },
+        { url: "/favicon.svg", revision: "9cebd6b65d0b92509c8e41fbab0a90fc" },
+        { url: "/file.svg", revision: "d09f95206c3fa0bb9bd9fefabfd0ea71" },
+        { url: "/globe.svg", revision: "2aaafa6a49b6563925fe440891e32717" },
+        { url: "/manifest.json", revision: "31ed5e027195e9dab23642e834565adf" },
+        { url: "/next.svg", revision: "8e061864f388b47f33a1c3780831193e" },
+        { url: "/offline", revision: "jt21rPV_ANoYZ5LR6NZTb" },
+        {
+          url: "/site.webmanifest",
+          revision: "9090e674d41e265d5f03c180fe5f6721",
+        },
+        { url: "/vercel.svg", revision: "c0af2f507b369b085b35ef4bbe3bcf1e" },
+        {
+          url: "/web-app-manifest-192x192.png",
+          revision: "8fb323041851752cd1b44fa7d7750303",
+        },
+        {
+          url: "/web-app-manifest-512x512.png",
+          revision: "4961ecc5660fb2a558c3fed2b418a8ce",
+        },
+        { url: "/window.svg", revision: "a2760511c65806022ad20adf74370ff3" },
+      ],
+      { ignoreURLParametersMatching: [/^utm_/, /^fbclid$/] },
+    ),
+    e.cleanupOutdatedCaches(),
+    e.registerRoute(
+      "/",
+      new e.NetworkFirst({
+        cacheName: "start-url",
+        plugins: [
+          {
+            cacheWillUpdate: async ({ response: e }) =>
+              e && "opaqueredirect" === e.type
+                ? new Response(e.body, {
+                    status: 200,
+                    statusText: "OK",
+                    headers: e.headers,
+                  })
+                : e,
+          },
+          {
+            handlerDidError: async ({ request: e }) =>
+              "undefined" != typeof self ? self.fallback(e) : Response.error(),
+          },
+        ],
+      }),
+      "GET",
+    ),
+    e.registerRoute(
+      /^https:\/\/fonts\.(?:gstatic)\.com\/.*/i,
+      new e.CacheFirst({
+        cacheName: "google-fonts-webfonts",
+        plugins: [
+          new e.ExpirationPlugin({ maxEntries: 4, maxAgeSeconds: 31536e3 }),
+          {
+            handlerDidError: async ({ request: e }) =>
+              "undefined" != typeof self ? self.fallback(e) : Response.error(),
+          },
+        ],
+      }),
+      "GET",
+    ),
+    e.registerRoute(
+      /^https:\/\/fonts\.(?:googleapis)\.com\/.*/i,
+      new e.StaleWhileRevalidate({
+        cacheName: "google-fonts-stylesheets",
+        plugins: [
+          new e.ExpirationPlugin({ maxEntries: 4, maxAgeSeconds: 604800 }),
+          {
+            handlerDidError: async ({ request: e }) =>
+              "undefined" != typeof self ? self.fallback(e) : Response.error(),
+          },
+        ],
+      }),
+      "GET",
+    ),
+    e.registerRoute(
+      /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
+      new e.StaleWhileRevalidate({
+        cacheName: "static-font-assets",
+        plugins: [
+          new e.ExpirationPlugin({ maxEntries: 4, maxAgeSeconds: 604800 }),
+          {
+            handlerDidError: async ({ request: e }) =>
+              "undefined" != typeof self ? self.fallback(e) : Response.error(),
+          },
+        ],
+      }),
+      "GET",
+    ),
+    e.registerRoute(
+      /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
+      new e.StaleWhileRevalidate({
+        cacheName: "static-image-assets",
+        plugins: [
+          new e.ExpirationPlugin({ maxEntries: 64, maxAgeSeconds: 2592e3 }),
+          {
+            handlerDidError: async ({ request: e }) =>
+              "undefined" != typeof self ? self.fallback(e) : Response.error(),
+          },
+        ],
+      }),
+      "GET",
+    ),
+    e.registerRoute(
+      /\/_next\/static.+\.js$/i,
+      new e.CacheFirst({
+        cacheName: "next-static-js-assets",
+        plugins: [
+          new e.ExpirationPlugin({ maxEntries: 64, maxAgeSeconds: 86400 }),
+          {
+            handlerDidError: async ({ request: e }) =>
+              "undefined" != typeof self ? self.fallback(e) : Response.error(),
+          },
+        ],
+      }),
+      "GET",
+    ),
+    e.registerRoute(
+      /\/_next\/image\?url=.+$/i,
+      new e.StaleWhileRevalidate({
+        cacheName: "next-image",
+        plugins: [
+          new e.ExpirationPlugin({ maxEntries: 64, maxAgeSeconds: 86400 }),
+          {
+            handlerDidError: async ({ request: e }) =>
+              "undefined" != typeof self ? self.fallback(e) : Response.error(),
+          },
+        ],
+      }),
+      "GET",
+    ),
+    e.registerRoute(
+      /\.(?:mp3|wav|ogg)$/i,
+      new e.CacheFirst({
+        cacheName: "static-audio-assets",
+        plugins: [
+          new e.RangeRequestsPlugin(),
+          new e.ExpirationPlugin({ maxEntries: 32, maxAgeSeconds: 86400 }),
+          {
+            handlerDidError: async ({ request: e }) =>
+              "undefined" != typeof self ? self.fallback(e) : Response.error(),
+          },
+        ],
+      }),
+      "GET",
+    ),
+    e.registerRoute(
+      /\.(?:mp4|webm)$/i,
+      new e.CacheFirst({
+        cacheName: "static-video-assets",
+        plugins: [
+          new e.RangeRequestsPlugin(),
+          new e.ExpirationPlugin({ maxEntries: 32, maxAgeSeconds: 86400 }),
+          {
+            handlerDidError: async ({ request: e }) =>
+              "undefined" != typeof self ? self.fallback(e) : Response.error(),
+          },
+        ],
+      }),
+      "GET",
+    ),
+    e.registerRoute(
+      /\.(?:js)$/i,
+      new e.StaleWhileRevalidate({
+        cacheName: "static-js-assets",
+        plugins: [
+          new e.ExpirationPlugin({ maxEntries: 48, maxAgeSeconds: 86400 }),
+          {
+            handlerDidError: async ({ request: e }) =>
+              "undefined" != typeof self ? self.fallback(e) : Response.error(),
+          },
+        ],
+      }),
+      "GET",
+    ),
+    e.registerRoute(
+      /\.(?:css|less)$/i,
+      new e.StaleWhileRevalidate({
+        cacheName: "static-style-assets",
+        plugins: [
+          new e.ExpirationPlugin({ maxEntries: 32, maxAgeSeconds: 86400 }),
+          {
+            handlerDidError: async ({ request: e }) =>
+              "undefined" != typeof self ? self.fallback(e) : Response.error(),
+          },
+        ],
+      }),
+      "GET",
+    ),
+    e.registerRoute(
+      /\/_next\/data\/.+\/.+\.json$/i,
+      new e.StaleWhileRevalidate({
+        cacheName: "next-data",
+        plugins: [
+          new e.ExpirationPlugin({ maxEntries: 32, maxAgeSeconds: 86400 }),
+          {
+            handlerDidError: async ({ request: e }) =>
+              "undefined" != typeof self ? self.fallback(e) : Response.error(),
+          },
+        ],
+      }),
+      "GET",
+    ),
+    e.registerRoute(
+      /\.(?:json|xml|csv)$/i,
+      new e.NetworkFirst({
+        cacheName: "static-data-assets",
+        plugins: [
+          new e.ExpirationPlugin({ maxEntries: 32, maxAgeSeconds: 86400 }),
+          {
+            handlerDidError: async ({ request: e }) =>
+              "undefined" != typeof self ? self.fallback(e) : Response.error(),
+          },
+        ],
+      }),
+      "GET",
+    ),
+    e.registerRoute(
+      ({ sameOrigin: e, url: { pathname: s } }) =>
+        !(!e || s.startsWith("/api/auth/callback") || !s.startsWith("/api/")),
+      new e.NetworkFirst({
+        cacheName: "apis",
+        networkTimeoutSeconds: 10,
+        plugins: [
+          new e.ExpirationPlugin({ maxEntries: 16, maxAgeSeconds: 86400 }),
+          {
+            handlerDidError: async ({ request: e }) =>
+              "undefined" != typeof self ? self.fallback(e) : Response.error(),
+          },
+        ],
+      }),
+      "GET",
+    ),
+    e.registerRoute(
+      ({ request: e, url: { pathname: s }, sameOrigin: n }) =>
+        "1" === e.headers.get("RSC") &&
+        "1" === e.headers.get("Next-Router-Prefetch") &&
+        n &&
+        !s.startsWith("/api/"),
+      new e.NetworkFirst({
+        cacheName: "pages-rsc-prefetch",
+        plugins: [
+          new e.ExpirationPlugin({ maxEntries: 32, maxAgeSeconds: 86400 }),
+          {
+            handlerDidError: async ({ request: e }) =>
+              "undefined" != typeof self ? self.fallback(e) : Response.error(),
+          },
+        ],
+      }),
+      "GET",
+    ),
+    e.registerRoute(
+      ({ request: e, url: { pathname: s }, sameOrigin: n }) =>
+        "1" === e.headers.get("RSC") && n && !s.startsWith("/api/"),
+      new e.NetworkFirst({
+        cacheName: "pages-rsc",
+        plugins: [
+          new e.ExpirationPlugin({ maxEntries: 32, maxAgeSeconds: 86400 }),
+          {
+            handlerDidError: async ({ request: e }) =>
+              "undefined" != typeof self ? self.fallback(e) : Response.error(),
+          },
+        ],
+      }),
+      "GET",
+    ),
+    e.registerRoute(
+      ({ url: { pathname: e }, sameOrigin: s }) => s && !e.startsWith("/api/"),
+      new e.NetworkFirst({
+        cacheName: "pages",
+        plugins: [
+          new e.ExpirationPlugin({ maxEntries: 32, maxAgeSeconds: 86400 }),
+          {
+            handlerDidError: async ({ request: e }) =>
+              "undefined" != typeof self ? self.fallback(e) : Response.error(),
+          },
+        ],
+      }),
+      "GET",
+    ),
+    e.registerRoute(
+      ({ sameOrigin: e }) => !e,
+      new e.NetworkFirst({
+        cacheName: "cross-origin",
+        networkTimeoutSeconds: 10,
+        plugins: [
+          new e.ExpirationPlugin({ maxEntries: 32, maxAgeSeconds: 3600 }),
+          {
+            handlerDidError: async ({ request: e }) =>
+              "undefined" != typeof self ? self.fallback(e) : Response.error(),
+          },
+        ],
+      }),
+      "GET",
+    );
+});
--- src/app/layout.js
+++ src/app/layout.js
@@ -17,6 +17,13 @@ export default function RootLayout({ children }) {
           name="description"
           content="Analyze toothpaste ingredients for safer, smarter choices with PastePick"
         />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#2563eb" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
+        <link rel="apple-touch-icon" href="/web-app-manifest-192x192.png" />
       </head>
       <body className={inter.className} suppressHydrationWarning>
         <ThemeProvider>
